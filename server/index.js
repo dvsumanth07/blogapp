756cfaee8486b2bdfcf7da833615344ba1d3111c
dvsumanth07 <dongavenkatasumanth.20.it@anits.edu.in>
2023-12-31 17:34:48 +0530

    added single post page

--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const fs = require("fs");
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.use("/uploads", express.static(__dirname + "/uploads"));
 mongoose.connect(
   "mongodb+srv://dvsumanth78:sozXfJDr6hDidAJi@cluster0.iqbptrj.mongodb.net/"
 );
@@ -42,7 +45,10 @@ app.post("/login", async (req, res) => {
   if (passOk) {
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie("token", token).json("ok");
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
     });
   } else {
     res.status(400).json("Invalid Credentials");
@@ -59,7 +65,37 @@ app.get("/profile", (req, res) => {
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
 });
+app.post("/post", upload.single("file"), async (req, res) => {
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+app.get("/post", async (req, res) => {
+  const posts = await Post.find()
+    .populate("author", ["username"])
+    .sort({ createdAt: -1 })
+    .limit(20);
+  res.json(posts);
+});
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
 app.listen(4000);
-//sozXfJDr6hDidAJi
-//dvsumanth78
-//mongodb+srv://dvsumanth78:<password>@cluster0.iqbptrj.mongodb.net/
