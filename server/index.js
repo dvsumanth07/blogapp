e02ab3571ac13885441c5fe30e857639e33c20a6
dvsumanth07 <dongavenkatasumanth.20.it@anits.edu.in>
2024-01-04 11:00:02 +0530

    created api for updating post

--- server/index.js
+++ server/index.js
@@ -93,9 +93,37 @@ app.get("/post", async (req, res) => {
     .limit(20);
   res.json(posts);
 });
+app.put("/post", upload.single("file"), async (req, res) => {
+  const newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("You are not author");
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+    res.json(postDoc);
+  });
+});
 app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
   const postDoc = await Post.findById(id).populate("author", ["username"]);
   res.json(postDoc);
 });
+
 app.listen(4000);
